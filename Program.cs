c6106b357f14bd91ae8fa393fa2b8feb1502ef77
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-24 19:08:28 +0300

    Task 47

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+//Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+double[,] CreateRandom2DArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a minimum possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a maximum possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    double[,] array = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = Math.Round(new Random().Next(minValue, maxValue) + new Random().NextDouble(), 2);
+        }
+    }
+    return array;
+}
+
+void Show2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+            
+            Console.WriteLine();
+        }
+    }
+}
+
+double[,] my2DArray = CreateRandom2DArray();
+Show2DArray(my2DArray);
+*/
