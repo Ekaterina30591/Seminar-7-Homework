c5dedd4e8e36d46ec6c30a225dbe4870ffb696fd
Ekaterina Kloss <kloss-kate@mail.ru>
2022-12-24 22:16:16 +0300

    Task 3 and amendments

--- Program.cs
+++ Program.cs
@@ -31,15 +31,15 @@ void Show2DArray(double[,] array)
         {
             Console.Write(array[i,j] + "\t");
             
-            Console.WriteLine();
         }
+
+        Console.WriteLine();
     }
 }
 
 double[,] my2DArray = CreateRandom2DArray();
 Show2DArray(my2DArray);
 */
-
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 //и возвращает значение этого элемента или же указание, что такого элемента нет.
 /*
@@ -86,24 +86,80 @@ void ElementPosition(int[,] array)
     int row = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Input column number of the element: ");
     int column = Convert.ToInt32(Console.ReadLine());
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
+
+     if (row < array.GetLength(0) || column < array.GetLength(1))
         {
-            if (row < array.GetLength(0) || column < array.GetLength(1))
-            {
-               Console.WriteLine ($"The element in row {row} and column {column} is equal to {array[row-1,column-1]}");
-            }
-            else
-            {
-                Console.WriteLine("The element does not exist in the array");
-            }
+               Console.WriteLine ($"The element in row {row} and column {column} is {array[row-1,column-1]}");
         }
+     else
+        {
+               Console.WriteLine("The element does not exist in the array");
+        }
+        
     }
-}
 
 int[,] my2DArray = CreateRandom2dArray();
 Show2dArray(my2DArray);
 ElementPosition(my2DArray);
 */
+/*
+//Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
 
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a minimum possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a maximum possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+void AverageinColumn (int[,]array)
+{
+     
+     for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        double average = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = (sum + array[i,j]);
+        }
+        average = sum/array.GetLength(0);
+        Console.Write($"{average}; ");
+    
+    }
+}
+
+int[,] my2DArray = CreateRandom2dArray();
+Show2dArray(my2DArray);
+AverageinColumn(my2DArray);
+*/
